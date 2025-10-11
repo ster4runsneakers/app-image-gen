0721e7c7d447e4274bf1fdcfd5bc14b050bd1cd1
ster4runsneakers <cornerstore553@gmail.com>
2025-10-11 20:02:49 +0300

    Implement service worker for caching assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,26 @@
+const CACHE = 'image-gen-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './manifest.webmanifest',
+  './static/icons/icon-192.png',
+  './static/icons/icon-512.png'
+];
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE && caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res=>{
+      const copy=res.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)).catch(()=>{});
+      return res;
+    }).catch(()=> r || new Response('Offline', {status:503})))
+  );
+});
 
